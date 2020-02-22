9187eed3d588cc3f0719dc9f1590c6caadd62873
BoD <BoD@JRAF.org>
2020-02-22 19:11:15 +0100

    Improve error handling

--- src/main/kotlin/org/jraf/appstoreratingrss/main/Main.kt
+++ src/main/kotlin/org/jraf/appstoreratingrss/main/Main.kt
@@ -3,8 +3,11 @@ package org.jraf.appstoreratingrss.main
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.DefaultHeaders
+import io.ktor.features.StatusPages
 import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.http.withCharset
+import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
@@ -12,6 +15,8 @@ import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import org.jraf.klibappstorerating.AppStore
 import org.jraf.klibappstorerating.KLibAppStoreRating
+import org.jraf.klibappstorerating.RatingRetrievalException
+import java.io.FileNotFoundException
 import java.text.SimpleDateFormat
 import java.util.Date
 
@@ -25,12 +30,37 @@ private const val PATH_APP_ID = "appId"
 private const val STORE_ID_GOOGLE_PLAY_STORE = "googlePlayStore"
 private const val STORE_ID_APPLE_APP_STORE = "appleAppStore"
 
+private const val APP_URL = "https://app-store-rating-rss.herokuapp.com"
+
 private val GUID_TIME_FORMAT = SimpleDateFormat("YYYY-MM-dd")
 
 fun main() {
     val port = System.getenv(ENV_PORT)?.toInt() ?: DEFAULT_PORT
     embeddedServer(Netty, port) {
         install(DefaultHeaders)
+
+        install(StatusPages) {
+            status(HttpStatusCode.NotFound) {
+                call.respondText(
+                    text = "Usage: $APP_URL/<$STORE_ID_GOOGLE_PLAY_STORE|$STORE_ID_APPLE_APP_STORE>/<app id>",
+                    status = it
+                )
+            }
+
+            exception<IllegalArgumentException> { exception ->
+                call.respond(HttpStatusCode.BadRequest, exception.message ?: "Bad request")
+            }
+            exception<RatingRetrievalException> { exception ->
+                val cause = exception.cause
+                call.respond(
+                    HttpStatusCode.BadRequest, if (cause is FileNotFoundException) {
+                        "Could not retrieve the rating for this app:\n${cause.message} not found"
+                    } else {
+                        exception.message ?: "Could not retrieve the rating for this app"
+                    }
+                )
+            }
+        }
 
         routing {
             get("{$PATH_APP_STORE_ID}/{$PATH_APP_ID}") {
@@ -39,11 +69,9 @@ fun main() {
                 val appStore = when (appStoreId) {
                     STORE_ID_GOOGLE_PLAY_STORE -> AppStore.GOOGLE_PLAY_STORE
                     STORE_ID_APPLE_APP_STORE -> AppStore.APPLE_APP_STORE
-                    else -> throw IllegalArgumentException("Unknown app store")
+                    else -> throw IllegalArgumentException("Unknown app store, authorized values are:\n- $STORE_ID_GOOGLE_PLAY_STORE\n- $STORE_ID_APPLE_APP_STORE")
                 }
-                val rating = KLibAppStoreRating.retrieveRating(
-                    appStore, appId
-                )
+                val rating = KLibAppStoreRating.retrieveRating(appStore, appId)
 
                 call.respondText(getRss(appStore, appId, rating), ContentType.Application.Rss.withCharset(Charsets.UTF_8))
             }
